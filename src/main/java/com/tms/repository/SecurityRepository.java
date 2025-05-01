e3f57907b513517ba5bb61c76eb016e76439057a
sergey <berdnikausiarhei@gmail.com>
2025-05-01 18:57:43 +0300

    Final-project-news-blog
    Adds repositories

---
+++ src/main/java/com/tms/repository/SecurityRepository.java
@@ -0,0 +1,9 @@
+package com.tms.repository;
+
+import com.tms.model.entity.Security;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SecurityRepository extends JpaRepository<Security, Long> {
+}
---
+++ src/main/java/com/tms/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tms.repository;
+
+import com.tms.model.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+}
